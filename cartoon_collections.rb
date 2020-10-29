a874da28fbfa5e25f99973191e9a2ede53b68076
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-29 22:56:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
 
   array.each_with_index do |dwarves, num|
-    puts "#{start + 1}.#{dwarves}"
+    puts "#{start + 1}. #{dwarves}"
   end
 end
